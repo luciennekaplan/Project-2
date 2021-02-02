4f42f0bcc1d1a3b66728b5c54d226a2a7ea77f99
MarcyVampQueen <savannahcordry@gmail.com>
2021-02-01 20:03:28 -0800

    handle multiple files

--- data/mongoImport.py
+++ data/mongoImport.py
@@ -9,12 +9,20 @@ csvpaths = glob.glob(os.path.join(os.path.dirname(__file__),"*.csv"))
 client = pymongo.MongoClient("mongodb://localhost:27017")
 db = client.employmentDB
 
-# Start with empty DB - CHANGE FOR EACH CSV
-db.data_analyst.delete_many({})
+# Start with empty database
+for col in db.list_collection_names():
+    db.drop_collection(col)
+
+# Function for grabbing the file name
+def nameTheCollection(name):
+    finalSlash = name.rfind('/')+1
+    return name[finalSlash:-4]
 
 # Begin import
-for csv in csvpaths:
-    with open(csv) as csvfile:
+for files in csvpaths:
+    # Make a new collection based on the file
+    collection = db.create_collection(nameTheCollection(files))
+    with open(files) as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
         # Skip over the header and print it
         csv_header = next(csvreader)
@@ -22,7 +30,7 @@ for csv in csvpaths:
 
         for row in csvreader:
             # Insert into DB - CHANGE FOR EACH CSV
-            db.data_analyst.insert_one(
+            collection.insert_one(
                 {
                     "title":row[1],
                     "salaryHigh":row[2],
@@ -43,4 +51,5 @@ for csv in csvpaths:
                 }
             )
     # Check rows imported matches CSV length
-    print(f"Rows imported: {db.data_analyst.count()}")
+    print(f"Rows imported: {collection.count()}")
+print(f"Created the following collections:\n {db.list_collection_names()}")
