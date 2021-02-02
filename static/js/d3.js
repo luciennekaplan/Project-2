79f72a07e99caee4dcea93cb05f96074c69703e7
Blashford <ashford.blake@gmail.com>
2021-02-01 20:59:41 -0800

    made industry bar chart

---
+++ static/js/d3.js
@@ -0,0 +1,69 @@
+// Here we set the svg area
+var svgWidth = 800;
+var svgHeight = 600;
+
+// and the margins so the axes labels will fit in the svg
+var margin = {
+    top: 50,
+    right: 60,
+    bottom: 100,
+    left: 100
+};
+
+// this is to set the height and width of the chart inside of the svg 
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// here is where we input the svg into the html
+var svg = d3.select("#bar")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+// This is where we add the group element to the svg, this is where our chart is going to go
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+var url = //whatever the route name is to access mongoDB
+d3.json(url).then(function(data) {
+    console.log(data)
+    var industries = {};
+    data.forEach((d, i) => {
+        if (d[i].industry in industries) {
+            industries[d[i].industry] += 1;
+        }
+        else {
+            industries[d[i].industry] = 1;
+        }
+    })
+    console.log(industries)
+
+    var xScale = d3.scaleBand()
+    .domain(Object.keys(industries))
+    .range([0, width])
+    .padding(0.1);
+
+    var yScale = d3.scaleLinear()
+    .domain([0, d3.max(Object.values(industries))])
+    .range([height, 0]);
+
+    var bottomAxis = d3.axisBottom(xScale);
+    var leftAxis = d3.axisLeft(yScale);
+
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chartGroup.selectAll(".bar")
+        .data(industries)
+        .enter()
+        .append("rect")
+        .classed("bar", true)
+        .attr("x", d => xScale(Object.keys(d)))
+        .attr("y", d => yScale(Object.values(d)))
+        .attr("width", xScale.bandwidth())
+        .attr("height", d => height - yScale(Object.values(d)));
+})
