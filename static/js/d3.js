7a8572978f29936154dda6e7a3863d6b6c4370e3
Blashford <ashford.blake@gmail.com>
2021-02-04 14:33:41 -0800

    Started working on transitions, got the axes and tooltips to update, working on the bars

--- static/js/d3.js
+++ static/js/d3.js
@@ -55,6 +55,7 @@ d3.json(url).then(function(data) {
     })
     console.log(sortedDicts)
     var topTen = sortedDicts.slice(0,10)
+    var tenToTwenty = sortedDicts.slice(10,20)
     console.log(topTen)
     var xScale = d3.scaleBand()
     .domain(topTen.map(d => d.name))
@@ -69,10 +70,12 @@ d3.json(url).then(function(data) {
     var leftAxis = d3.axisLeft(yScale);
     
     chartGroup.append("g")
+        .classed("yAxis", true)
         .call(leftAxis);
 
     chartGroup.append("g")
         .attr("transform", `translate(0, ${height})`)
+        .classed("xAxis", true)
         .call(bottomAxis)
         .selectAll("text")
         .attr("y", 0)
@@ -99,4 +102,62 @@ d3.json(url).then(function(data) {
         .attr("height", d => height - yScale(d.count))
         .on("mouseover", toolTip.show)
         .on("mouseout", toolTip.hide);
+
+    var topTenAxis = chartGroup.append("text")
+        .attr("transform", `translate(${(width / 2) - 30}, ${height + margin.top})`)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "16px")
+        .classed("active x", true) 
+        .text("1");
+
+    var tenToTwentyAxis = chartGroup.append("text")
+        .attr("transform", `translate(${(width / 2) - 20}, ${height + margin.top})`)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "16px")
+        .classed("inactive", true) // I also add the class "x" to the active x axis label so that I can access it later in the listeners
+        .text("2");
+
+    tenToTwentyAxis.on("click", function (d) {
+        topTenAxis.classed("inactive", true)
+            .classed("active x", false);
+        tenToTwentyAxis.classed("inactive", false)
+            .classed("active x", true);
+        console.log(tenToTwenty)
+        
+        xScale = d3.scaleBand()
+        .domain(tenToTwenty.map(d => d.name))
+        .range([0, width])
+        .padding(0.1);
+    
+        yScale = d3.scaleLinear()
+        .domain([0, d3.max(tenToTwenty, d => d.count)])
+        .range([height, 0]);
+    
+        bottomAxis = d3.axisBottom(xScale);
+        leftAxis = d3.axisLeft(yScale);
+
+        d3.select(".xAxis")
+            .transition()
+            .duration(500)
+            .call(bottomAxis)
+            .selectAll("text")
+            .attr("y", 0)
+            .attr("x", 9)
+            .attr("dy", ".35em")
+            .attr("transform", "rotate(25)")
+            .style("text-anchor", "start");
+
+        d3.select(".yAxis")
+            .transition()
+            .duration(500)
+            .call(leftAxis)
+
+        d3.selectAll(".indusbar")
+            .data(tenToTwenty)
+            .enter()
+            .attr("x", d => xScale(d.name))
+            .attr("y", d => yScale(d.count))
+            .attr("width", xScale.bandwidth())
+            .attr("height", d => height - yScale(d.count));
+    })
 })
