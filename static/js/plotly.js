2a633ea0084ec8d5ee9334401dfa9f149e069616
MarcyVampQueen <savannahcordry@gmail.com>
2021-02-06 13:13:59 -0800

    handling dropdown

--- static/js/plotly.js
+++ static/js/plotly.js
@@ -1,62 +1,81 @@
-var url = "/all"
+function handleSubmit() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
 
-d3.json(url).then(function (theData) {
-    console.log(theData);
-    data = []
-    for (const [key, value] of Object.entries(theData)) {
-        salary = value.map(d => (parseInt(d.salaryHigh) +
-            parseInt(d.salaryLow)) / 2);
-        var trace = {
-            x: salary,
-            name: key.slice(5),
-            type: 'histogram',
-            marker: {
-                color: pickColor(key)["fill"],
-                line: {
-                    color: pickColor(key)["line"],
-                    width: 1
-                }
-            },
-            opacity: 0.5
-        };
-        data.push(trace)
-    }
-    var layout = {
-        bargap: 0.05, 
-        bargroupgap: 0.2, 
-        barmode: "overlay", 
-        title: "Common Salaries", 
-        xaxis: {title: "Salary"}, 
-        yaxis: {title: "Count"},
-        legend: {
-            orientation:"h",
-            yanchor:"bottom",
-            y:1.02,
-            xanchor:"right",
-            x:1
-        }
-      };
-    Plotly.newPlot('bargraph2', data,layout);
+    // Select the input value from the form
+    var subject = d3.select("#dropdown-menu").node().value;
+    console.log(subject)
+    // Update the Dashboard!
 }
-    , function (error) {
-        console.log(error);
-    });
+
+function buildSalaryChart(url) {
+    d3.json(url).then(function (theData) {
+        console.log(theData);
+        data = [];
+        for (const [key, value] of Object.entries(theData)) {
+            salary = value.map(d => (parseInt(d.salaryHigh) +
+                parseInt(d.salaryLow)) / 2);
+            var trace = {
+                x: salary,
+                name: key.slice(5),
+                type: 'histogram',
+                marker: {
+                    color: pickColor(key)["fill"],
+                    line: {
+                        color: pickColor(key)["line"],
+                        width: 1
+                    }
+                },
+                opacity: 0.5
+            };
+            data.push(trace)
+        }
+        var layout = {
+            bargap: 0.05,
+            bargroupgap: 0.2,
+            title: "Common Salaries",
+            xaxis: { title: "Salary" },
+            yaxis: { title: "Count" },
+            legend: {
+                orientation: "h",
+                yanchor: "bottom",
+                y: 1.02,
+                xanchor: "right",
+                x: 1
+            }
+        };
+        Plotly.newPlot('bargraph2', data, layout);
+    }
+        , function (error) {
+            console.log(error);
+        });
+}
 
 function pickColor(type) {
     if (type === 'CleanDataAnalyst') {
-        return {"fill":"rgba(255, 100, 102, 0.7)",
-                "line":"rgba(255, 100, 102, 1)"}
+        return {
+            "fill": "rgba(255, 100, 102, 0.7)",
+            "line": "rgba(255, 100, 102, 1)"
+        };
     }
-    else if (type === 'CleanBusinessAnalyst'){
-        return {"fill":"rgba(100, 200, 102, 0.7)",
-                "line":"rgba(100, 200, 102, 1)"}
+    else if (type === 'CleanBusinessAnalyst') {
+        return {
+            "fill": "rgba(100, 200, 102, 0.7)",
+            "line": "rgba(100, 200, 102, 1)"
+        };
     }
-    else if (type === 'CleanDataEngineer'){
-        return {"fill":"rgba(100, 200, 255, 0.7)",
-                "line":"rgba(100, 200, 255, 1)"}
+    else if (type === 'CleanDataEngineer') {
+        return {
+            "fill": "rgba(100, 200, 255, 0.7)",
+            "line": "rgba(100, 200, 255, 1)"
+        };
     }
-    else if (type === 'CleanDataScientist'){
-        return {"fill":"rgba(255, 100, 255, 0.7)",
-                "line":"rgba(255, 100, 255, 1)"}
+    else if (type === 'CleanDataScientist') {
+        return {
+            "fill": "rgba(255, 100, 255, 0.7)",
+            "line": "rgba(255, 100, 255, 1)"
+        };
     }
 }
+buildSalaryChart("/all");
+d3.selectAll("#dropdown-menu").on("change", handleSubmit);
