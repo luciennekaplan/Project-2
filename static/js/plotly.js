04f9e17a086ed523f11a8af7fdce997a4061f3b4
MarcyVampQueen <savannahcordry@gmail.com>
2021-02-06 13:05:44 -0800

    simplified loop

--- static/js/plotly.js
+++ static/js/plotly.js
@@ -1,40 +1,62 @@
 var url = "/all"
 
 d3.json(url).then(function (theData) {
-    salaryData = theData.CleanDataAnalyst.map(
-        d => (parseInt(d.salaryHigh) + parseInt(d.salaryLow)) / 2);
-    salaryBiz = theData.CleanBusinessAnalyst.map(
-        d => (parseInt(d.salaryHigh) + parseInt(d.salaryLow)) / 2);
-
-    var trace1 = {
-        x: salaryBiz,
-        name: 'Business Analyst',
-        type: 'histogram',
-        marker: {
-            color: "rgba(255, 100, 102, 0.7)",
-            line: {
-                color: "rgba(255, 100, 102, 1)",
-                width: 1
-            }
-        },
-        opacity: 0.5
-    };
-    var trace2 = {
-        x: salaryData,
-        name: 'Data Analyst',
-        type: 'histogram',
-        marker: {
-            color: "rgba(100, 200, 102, 0.7)",
-            line: {
-                color: "rgba(100, 200, 102, 1)",
-                width: 1
-            }
-        },
-        opacity: 0.5
-    };
-    var data = [trace1, trace2];
-    Plotly.newPlot('bargraph2', data);
+    console.log(theData);
+    data = []
+    for (const [key, value] of Object.entries(theData)) {
+        salary = value.map(d => (parseInt(d.salaryHigh) +
+            parseInt(d.salaryLow)) / 2);
+        var trace = {
+            x: salary,
+            name: key.slice(5),
+            type: 'histogram',
+            marker: {
+                color: pickColor(key)["fill"],
+                line: {
+                    color: pickColor(key)["line"],
+                    width: 1
+                }
+            },
+            opacity: 0.5
+        };
+        data.push(trace)
+    }
+    var layout = {
+        bargap: 0.05, 
+        bargroupgap: 0.2, 
+        barmode: "overlay", 
+        title: "Common Salaries", 
+        xaxis: {title: "Salary"}, 
+        yaxis: {title: "Count"},
+        legend: {
+            orientation:"h",
+            yanchor:"bottom",
+            y:1.02,
+            xanchor:"right",
+            x:1
+        }
+      };
+    Plotly.newPlot('bargraph2', data,layout);
 }
     , function (error) {
         console.log(error);
     });
+
+function pickColor(type) {
+    if (type === 'CleanDataAnalyst') {
+        return {"fill":"rgba(255, 100, 102, 0.7)",
+                "line":"rgba(255, 100, 102, 1)"}
+    }
+    else if (type === 'CleanBusinessAnalyst'){
+        return {"fill":"rgba(100, 200, 102, 0.7)",
+                "line":"rgba(100, 200, 102, 1)"}
+    }
+    else if (type === 'CleanDataEngineer'){
+        return {"fill":"rgba(100, 200, 255, 0.7)",
+                "line":"rgba(100, 200, 255, 1)"}
+    }
+    else if (type === 'CleanDataScientist'){
+        return {"fill":"rgba(255, 100, 255, 0.7)",
+                "line":"rgba(255, 100, 255, 1)"}
+    }
+}
